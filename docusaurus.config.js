3c234e743e1b419473de98dc9d01b7ca0a0335a0
Dan Ziv <danz@starkware.co>
2022-02-21 15:04:51 +0200

    fix: test ci

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,7 +69,6 @@ const config = {
             position: 'left',
             label: 'Tutorial'
           },
-          // { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/starkware-libs/cairo-lang/tree/master/src/starkware/starknet',
             label: 'GitHub',
